6be95093ba0852e85e1a3d732debdee6e4048e69
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-29 13:45:27 +0300

    feat: file system rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+  if (!fs.existsSync('./files/wrongFilename.txt') || fs.existsSync('./files/properFilename.md')) {
+    throw new Error('FS operation failed')
+  }
+  fs.renameSync('./files/wrongFilename.txt', './files/properFilename.md')
 };
 
 await rename();
