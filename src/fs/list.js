9e87f4b6fd46920e095ad91fe3cb0afe401cd4aa
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-29 14:24:07 +0300

    feat: file system list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+
 const list = async () => {
-    // Write your code here 
+  fs.access('./files', (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.readdirSync('./files').forEach(file => {
+        console.log(file);
+      });
+    }
+  });
+
 };
 
 await list();
