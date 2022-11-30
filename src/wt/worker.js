d6ffaba401ff09461eea2ffa4c90e07bf911570c
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-30 14:20:52 +0300

    Update worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,25 +1,11 @@
 // n should be received from main thread
-import { Transform } from 'stream';
-
 import { workerData, parentPort } from 'worker_threads';
 
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
   parentPort.postMessage(nthFibonacci(workerData));
-  // const fibonacciOut = new Transform({
-  //   transform(chunk, encoding, callback) {
-  //     let num = Number(chunk);
-  //     // if (typeof num === Number) {
-  //       this.push(nthFibonacci(num) + '\n');
-  //     // } else {
-  //       // this.push('No a number!')
-  //     // }
-  //     callback();
-  //   }
-  // });
-  
-  // process.stdin.pipe(fibonacciOut).pipe(process.stdout);
+ 
 };
 
 sendResult();
