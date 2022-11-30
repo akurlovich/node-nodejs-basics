f5d225d81ed1d39aa5e70713ca59b283c3740a2a
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-30 09:15:53 +0300

    feat: CLI args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  if (process.argv.length < 3) {
+    console.log('No arguments!');
+    return;
+  };
+  let argsArray = [];
+  for (let i = 2; i < process.argv.length - 1; i++) {
+    if (process.argv[i].startsWith('--')) {
+      argsArray.push(`${process.argv[i]} is ${process.argv[i + 1]}`)
+    }
+  }
+  console.log(argsArray.join(', '))
 };
 
 parseArgs();
