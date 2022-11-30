daabaa4c00292824cf88c75f37727608e38db9e3
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-30 11:22:33 +0300

    feat: Streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = fs.createWriteStream(__dirname + '/files/fileToWrite.txt', 'UTF-8');
+  process.stdin.on("data", (data) => {
+    writableStream.write(data);
+  });
+  process.stdin.on("end", () => {
+    writableStream.end();
+  });
 };
 
 await write();
