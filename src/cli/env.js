78083f1f128d0427f8222c47ed258a1e35f6b07f
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-30 08:01:50 +0300

    feat: CLI env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envParse = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith('RSS_')) {
+      acc.push(`${key}=${value}`);
+    }
+    return acc;
+  }, [])
+  console.log(envParse.join('; '));
 };
 
 parseEnv();
