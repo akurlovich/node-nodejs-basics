528ecba33ee57aefc24b502fd7693fa29d2a5d48
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-30 10:28:35 +0300

    feat: Hash calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileBuffer = fs.readFileSync(__dirname + '/files/fileToCalculateHashFor.txt');
+  const hashSum = crypto.createHash('sha256');
+  hashSum.update(fileBuffer);
+
+  const hex = hashSum.digest('hex');
+
+  console.log(hex);
 };
 
 await calculateHash();
