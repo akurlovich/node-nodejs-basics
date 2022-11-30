f82a4b033f9651344c9da598b4a9885ca08b7dca
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-11-30 11:56:44 +0300

    feat: Zlib compress.js and decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  if (!fs.existsSync(__dirname + '/files/fileToCompress.txt')) {
+    throw new Error('FS operation failed')
+  }
+  fs.createReadStream(__dirname + '/files/fileToCompress.txt')
+  .pipe(zlib.createGzip())
+  .pipe(fs.createWriteStream(__dirname + '/files/archive.gz'))
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  if (!fs.existsSync(__dirname + '/archive.gz')) {
+    throw new Error('FS operation failed')
+  }
+  fs.createReadStream(__dirname + '/files/archive.gz')
+  .pipe(zlib.createUnzip())
+  .pipe(fs.createWriteStream(__dirname + '/files/fileToCompress.txt'))
 };
 
 await decompress();
