cd830bee8bc8ff8b5fe32cd8d8fc4547c71ea631
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2022-12-01 09:25:26 +0300

    feat: CP cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  if (args === undefined) {
+    args = '--writeSomethingToConsole';
+  }
+
+  const childProcess = spawn('node', [`${__dirname}/files/script.js`, ...args.split(' ')]);
+
+  process.stdin.on('data', (msg) => {
+    childProcess.stdin.write(msg);
+  });
+
+  childProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess('--arg1 --arg2 --arg3');
